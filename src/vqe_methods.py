13fc80e543b2a1a2a94b9678466e3112d5378991
holuntang <holuntang@vt.edu>
2019-03-01 19:10:22 -0500

    add ADAPT decomposition for fermionic

--- src/vqe_methods.py
+++ src/vqe_methods.py
@@ -6,10 +6,17 @@ import numpy as np
 import copy
 import random 
 import sys
+import csv
+import cirq
+import openfermioncirq
+from openfermioncirq import trotter
 
 import operator_pools
 import vqe_methods
 from tVQE import *
+
+from  openfermionprojectq  import  uccsd_trotter_engine, TimeEvolution
+from  projectq.backends  import CommandPrinter
 
 from openfermion import *
 
@@ -19,7 +26,7 @@ def adapt_vqe(geometry,
         multiplicity    = 1,
         charge          = 1,
         adapt_conver    = 'norm',
-        adapt_thresh    = 1e-3,
+        adapt_thresh    = 1e-4,
         theta_thresh    = 1e-7,
         adapt_maxiter   = 200,
         pool            = operator_pools.singlet_GSD(),
@@ -48,16 +55,14 @@ def adapt_vqe(geometry,
     #print(' CCSD energy    %20.16f au' %(molecule.ccsd_energy))
     print(' FCI energy     %20.16f au' %(molecule.fci_energy))
 
-    #Build p-h reference and map it to JW transform
-    reference_ket = scipy.sparse.csc_matrix(
-            openfermion.jw_configuration_state(
-                list(range(0,molecule.n_electrons)), molecule.n_qubits)).transpose()
-    reference_bra = reference_ket.transpose().conj()
-
     #JW transform Hamiltonian computed classically with OFPsi4
     hamiltonian_op = molecule.get_molecular_hamiltonian()
     hamiltonian = openfermion.transforms.get_sparse_operator(hamiltonian_op)
+    # print("H",hamiltonian)
   #  print(hamiltonian[:,240])
+    HJW = openfermion.transforms.jordan_wigner(hamiltonian_op)
+
+    # print(HJW)
 
     #Thetas
     parameters = []
@@ -68,12 +73,32 @@ def adapt_vqe(geometry,
     ansatz_ops = []     #SQ operator strings in the ansatz
     ansatz_mat = []     #Sparse Matrices for operators in ansatz
     
+    #Build p-h reference and map it to JW transform
+    reference_ket = scipy.sparse.csc_matrix(
+            openfermion.jw_configuration_state(
+                list(range(0,molecule.n_electrons)), molecule.n_qubits)).transpose()
+
+    # for n in range(molecule.n_electrons, molecule.n_qubits):
+    # 	reference_ket = 1/np.sqrt(2)*(np.exp(1j*0.5)*-1j*pool.spmat_ops[n].dot(reference_ket) + reference_ket)
+    # 	print(pool.fermi_ops[n])
+    
+    # reference_ket = 1/np.sqrt(2)*(np.exp(1j*0.5)*-1j*pool.spmat_ops[3].dot(reference_ket) + reference_ket)
+
+    print(reference_ket)
+
+    reference_bra = reference_ket.transpose().conj()
+
     print(" Start ADAPT-VQE algorithm")
     op_indices = []
     parameters = []
     curr_state = 1.0*reference_ket
 
-   # curr_state = 1/np.sqrt(2)*(pool.spmat_ops[49].dot(curr_state) + curr_state)
+
+    # for n in range(16,len(pool.spmat_ops)):
+    # 	curr_state = 1/np.sqrt(2)*(pool.spmat_ops[n].dot(curr_state) + curr_state)
+    # 	print(pool.fermi_ops[n])
+
+    # curr_state = 1/np.sqrt(2)*(pool.spmat_ops[49].dot(curr_state) + curr_state)
 
 
     print(" Now start to grow the ansatz")
@@ -127,24 +152,24 @@ def adapt_vqe(geometry,
                 opstring += str(t)
                 break
        
-            if abs(com) > adapt_thresh:
-                print(" %4i %40s %12.8f" %(op_trial, opstring, com) )
+            # if abs(com) > adapt_thresh:
+            #     print(" %4i %40s %12.8f" %(op_trial, opstring, com) )
 
             curr_norm += com*com
-         #   if abs(com) > abs(next_deriv) +1e-6:
-         #       next_deriv = com
-         #       next_index = op_trial
-
-            if abs(com) > abs(next_deriv) + 1e-6:
-                group = []
+            if abs(com) > abs(next_deriv) +1e-6:
                 next_deriv = com
                 next_index = op_trial
-                Sign = []
-            elif (next_deriv) > 1e-6:
-                if abs(abs(com)-abs(next_deriv)) < 1e-6:
-                    group.append(op_trial)
-                    sign = com*next_deriv/abs(com*next_deriv)
-                    Sign.append(sign)
+
+            # if abs(com) > abs(next_deriv) + 1e-6:
+            #     group = []
+            #     next_deriv = com
+            #     next_index = op_trial
+            #     Sign = []
+            # elif (next_deriv) > 1e-6:
+            #     if abs(abs(com)-abs(next_deriv)) < 1e-6:
+            #         group.append(op_trial)
+            #         sign = com*next_deriv/abs(com*next_deriv)
+            #         Sign.append(sign)
         
         print(Sign)
 
@@ -170,16 +195,33 @@ def adapt_vqe(geometry,
             print(" Number of operators in ansatz: ", len(ansatz_ops))
             print(" *Finished: %20.12f" % trial_model.curr_energy)
             print(" -----------Final ansatz----------- ")
-            print(" %4s %40s %12s" %("#","Term","Coeff"))
+            print(" %4s %30s %12s" %("Term","Coeff","#"))
             for si in range(len(ansatz_ops)):
                 s = ansatz_ops[si]
                 opstring = ""
                 for t in s.terms:
                     opstring += str(t)
                     break
-                print(" %4i %40s %12.8f" %(si, opstring, parameters[si]) )
-            break
+                print(" %4s %20f %10s" %(s, parameters[si], si) )
+                print(" ")
+
+                # compiler_engine = uccsd_trotter_engine( compiler_backend=CommandPrinter() )
+                # wavefunction = compiler_engine.allocate_qureg(molecule.n_qubits)
         
+                # # H = openfermion.transforms.get_interaction_operator(1j*T)
+                # H = openfermion.transforms.jordan_wigner(1j*s)
+        
+                # # Trotter step parameters.
+                # time = parameters[si]
+        
+                # evolution_operator = TimeEvolution(time,H)
+        
+                # evolution_operator | wavefunction
+        
+                # compiler_engine.flush()
+
+            break
+
         new_op = pool.fermi_ops[next_index]
         new_mat = pool.spmat_ops[next_index]
 
@@ -188,30 +230,57 @@ def adapt_vqe(geometry,
             new_mat += Sign[n]*pool.spmat_ops[group[n]]
 
         print(" Add operator %4i" %next_index)
+
+        for n in range(n_iter):
+        	parameters[n] = 0
+
         for n in group:
             print(" Add operator %4i " %n)
         parameters.insert(0,0)
         ansatz_ops.insert(0,new_op)
         ansatz_mat.insert(0,new_mat)
+        # print(openfermion.transforms.jordan_wigner(new_op))
+
         
         trial_model = tUCCSD(hamiltonian, ansatz_mat, reference_ket, parameters)
         
 
         opt_result = scipy.optimize.minimize(trial_model.energy, parameters, jac=trial_model.gradient, 
                 options = min_options, method = 'BFGS', callback=trial_model.callback)
+
+        # print(ansatz_ops)
     
         parameters = list(opt_result['x'])
         curr_state = trial_model.prepare_state(parameters)
+        # print(" new state ",curr_state)
         print(" Finished: %20.12f" % trial_model.curr_energy)
         print(" -----------New ansatz----------- ")
-        print(" %4s %40s %12s" %("#","Term","Coeff"))
+        # print(curr_state)
+        print(" %4s %30s %12s" %("Term","Coeff","#"))
         for si in range(len(ansatz_ops)):
             s = ansatz_ops[si]
             opstring = ""
             for t in s.terms:
                 opstring += str(t)
                 break
-            print(" %4i %40s %12.8f" %(si, opstring, parameters[si]) )
+            print(" %4s %20f %10s" %(s, parameters[si], si) )
+            print(" ")
+
+        # H = openfermion.get_diagonal_coulomb_hamiltonian(1j*T)
+
+        # print(H)
+
+
+        # if n_iter == 0:
+
+        #     with open('BeH2_q_adapt_ng.csv', mode='w') as h4:
+        #         h4 = csv.writer(h4, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #         h4.writerow([molecule.fci_energy-trial_model.curr_energy, n_iter])
+        # else:
+        #     with open('BeH2_q_adapt_ng.csv', mode='a') as h4:
+        #         h4 = csv.writer(h4, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #         h4.writerow([molecule.fci_energy-trial_model.curr_energy, n_iter])
+
 
 # }}}
 
@@ -345,6 +414,8 @@ def test_random(geometry,
         print(" Measure commutators:")
         sig = hamiltonian.dot(curr_state)
         for op_trial in range(pool.n_ops):
+
+            opA = pool.spmat_ops[op_trial]
             
             com = 2*(curr_state.transpose().conj().dot(opA.dot(sig))).real
             assert(com.shape == (1,1))
@@ -420,6 +491,16 @@ def test_random(geometry,
                 opstring += str(t)
                 break
             print(" %4i %40s %12.8f" %(si, opstring, parameters[si]) )
+
+        # if n_iter == 0:
+
+        #     with open('h4_q_adapt_rand.csv', mode='w') as h4:
+        #         h4 = csv.writer(h4, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #         h4.writerow([molecule.fci_energy-trial_model.curr_energy, n_iter])
+        # else:
+        #     with open('h4_q_adapt_rand.csv', mode='a') as h4:
+        #         h4 = csv.writer(h4, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #         h4.writerow([molecule.fci_energy-trial_model.curr_energy, n_iter])
 
     return
 # }}}
