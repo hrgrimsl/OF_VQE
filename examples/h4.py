19dfcadf486ef46006ac93be7bdc71c3a87b63a9
holuntang <holuntang@vt.edu>
2019-07-16 15:57:41 -0400

    q_adapt

--- examples/h4.py
+++ examples/h4.py
@@ -5,4 +5,4 @@ r = 1.5
 geometry = [('H', (0,0,1*r)), ('H', (0,0,2*r)), ('H', (0,0,3*r)), ('H', (0,0,4*r))]
 
 
-vqe_methods.adapt_vqe(geometry,pool = operator_pools.singlet_SD())
+vqe_methods.q_adapt_vqe(geometry,pool = operator_pools.qubits())
