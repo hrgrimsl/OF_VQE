0ce978a8c248b7a14af17bb2294e740fade4e151
holuntang <46738761+holuntang@users.noreply.github.com>
2019-07-22 17:29:04 -0400

    Add files via upload

---
+++ src/qaoa_methods.py
@@ -0,0 +1,263 @@
+import scipy
+import openfermion
+import networkx as nx
+import os
+import numpy as np
+import copy
+import random
+import sys
+
+import operator_pools
+from tVQE import *
+
+from openfermion import *
+
+
+def qaoa(n,
+         g,
+         adapt_thresh=1e-3,
+         theta_thresh=1e-7,
+         layer = 1,
+         pool=operator_pools.qaoa(),
+         ):
+    # {{{
+
+    G = g
+    print('G', G)
+    pool.init(n, G)
+    pool.generate_SparseMatrix()
+
+    w, v = scipy.sparse.linalg.eigs(pool.cost_mat[0]*1j)
+    GS = scipy.sparse.csc_matrix(v[:,w.argmin()]).transpose().conj()
+    GS_energy = min(w)
+
+    print('energy:', GS_energy.real)
+    print('maxcut objective:', GS_energy.real + pool.shift.real )
+
+    reference_ket = scipy.sparse.csc_matrix(
+        np.full((2**n, 1), 1/np.sqrt(2**n))
+    )
+    reference_bra = reference_ket.transpose().conj()
+
+    # Thetas
+    parameters = []
+
+    print(" Start QAOA algorithm")
+    curr_state = 1.0 * reference_ket
+
+    ansatz_ops = []
+    ansatz_mat = []
+
+    for p in range(0, layer):
+        print(" --------------------------------------------------------------------------")
+        print("                                  QAOA: ", p+1)
+        print(" --------------------------------------------------------------------------")
+
+        ansatz_ops.insert(0, pool.cost_ops[0])
+        ansatz_mat.insert(0, pool.cost_mat[0])
+
+        ansatz_ops.insert(0, pool.mixer_ops[0])
+        ansatz_mat.insert(0, pool.mixer_mat[0])
+
+        parameters.insert(0, 1)
+        parameters.insert(0, 1)
+
+        min_options = {'gtol': theta_thresh, 'disp': False}
+
+        trial_model = tUCCSD(pool.cost_mat[0], ansatz_mat, reference_ket, parameters)
+
+        opt_result = scipy.optimize.minimize(trial_model.energy, parameters, jac=trial_model.gradient,
+                                             options=min_options, method='Nelder-Mead', callback=trial_model.callback)
+
+        parameters = list(opt_result['x'])
+        curr_state = trial_model.prepare_state(parameters)
+        print(" Finished: %20.12f" % trial_model.curr_energy)
+        print(' maxcut objective:', trial_model.curr_energy + pool.shift)
+
+
+def q_adapt_vqe(n,
+         g,
+         adapt_thresh=1e-3,
+         theta_thresh=1e-7,
+                layer = 1,
+         adapt_maxiter = 1,
+         pool=operator_pools.qaoa(),
+                ):
+    # {{{
+
+    G = g
+    print('G', G)
+    pool.init(n, G)
+    pool.generate_SparseMatrix()
+
+    hamiltonian = pool.cost_mat[0]*1j
+
+    w, v = scipy.sparse.linalg.eigs(pool.cost_mat[0] * 1j)
+    GS = scipy.sparse.csc_matrix(v[:, w.argmin()]).transpose().conj()
+    GS_energy = min(w)
+
+    print('energy:', GS_energy.real)
+    print('maxcut objective:', GS_energy.real + pool.shift.real)
+
+    reference_ket = scipy.sparse.csc_matrix(
+        np.full((2 ** n, 1), 1 / np.sqrt(2 ** n))
+    )
+    reference_bra = reference_ket.transpose().conj()
+
+    # Thetas
+    parameters = []
+
+    print(" Start ADAPT algorithm")
+    curr_state = 1.0 * reference_ket
+
+    ansatz_ops = []
+    ansatz_mat = []
+
+    print(" Now start to grow the ansatz")
+    for n_iter in range(0, adapt_maxiter):
+
+        print("\n\n\n")
+        print(" --------------------------------------------------------------------------")
+        print("                         ADAPT-VQE iteration: ", n_iter)
+        print(" --------------------------------------------------------------------------")
+        next_index = None
+        next_deriv = 0
+        curr_norm = 0
+
+        print(" Check each new operator for coupling")
+        group = []
+        print(" Measure commutators:")
+        sig = hamiltonian.dot(curr_state)
+
+        for op_trial in range(pool.n_ops):
+
+            opA = pool.spmat_ops[op_trial]
+            com = 2 * (curr_state.transpose().conj().dot(opA.dot(sig))).real
+            assert (com.shape == (1, 1))
+            com = com[0, 0]
+            assert (np.isclose(com.imag, 0))
+            com = com.real
+
+            opstring = ""
+            for t in pool.fermi_ops[op_trial].terms:
+                opstring += str(t)
+                break
+
+            # if abs(com) > adapt_thresh:
+            print(" %4i %40s %12.8f" % (op_trial, opstring, com))
+
+            curr_norm += com * com
+
+            if abs(com) > abs(next_deriv) + 1e-9:
+                next_deriv = com
+                next_index = op_trial
+
+        curr_norm = np.sqrt(curr_norm)
+
+        min_options = {'gtol': theta_thresh, 'disp': False}
+
+        max_of_com = next_deriv
+        print(" Norm of <[A,H]> = %12.8f" % curr_norm)
+        print(" Max  of <[A,H]> = %12.8f" % max_of_com)
+
+        converged = False
+        if adapt_conver == "norm":
+            if curr_norm < adapt_thresh:
+                converged = True
+        else:
+            print(" FAIL: Convergence criterion not defined")
+            exit()
+
+        if converged:
+            pickle.dump(ansatz_ops, open('./h4_ansatz.p', 'wb'))
+            print(" Ansatz Growth Converged!")
+            print(" Number of operators in ansatz: ", len(ansatz_ops))
+            print(" *Finished: %20.12f" % trial_model.curr_energy)
+            print(" -----------Final ansatz----------- ")
+            print(" %4s %30s %12s" % ("Term", "Coeff", "#"))
+            new_state = reference_ket
+            E_step = []
+            for k in reversed(range(0, len(parameters))):
+                new_state = scipy.sparse.linalg.expm_multiply((parameters[k] * ansatz_mat[k]), new_state)
+                E_step.append(new_state.transpose().conj().dot(hamiltonian.dot(new_state))[0, 0].real)
+                print(len(parameters))
+                print(k)
+                print('Energy step', float(E_step[len(parameters) - k - 1]))
+                print("")
+            for si in range(len(ansatz_ops)):
+                s = ansatz_ops[si]
+                opstring = ""
+                for t in s.terms:
+                    opstring += str(t)
+                    break
+                print(" %4s %20f %10s" % (s, parameters[si], si))
+                print(" ")
+
+                compiler_engine = uccsd_trotter_engine(compiler_backend=CommandPrinter())
+                wavefunction = compiler_engine.allocate_qureg(n)
+
+                H = 1j * s  # Qubits -pool
+
+                # Trotter step parameters.
+                time = parameters[si]
+
+                evolution_operator = TimeEvolution(time, H)
+
+                evolution_operator | wavefunction
+
+                compiler_engine.flush()
+
+            break
+
+            break
+
+        new_op = pool.fermi_ops[next_index]
+        new_mat = pool.spmat_ops[next_index]
+
+        for n in range(len(group)):
+            new_op += Sign[n] * pool.fermi_ops[group[n]]
+            new_mat += Sign[n] * pool.spmat_ops[group[n]]
+
+        print(" Add operator %4i" % next_index)
+
+        # for n in range(n_iter):
+        #     parameters[n] = 0
+
+        for n in group:
+            print(" Add operator %4i " % n)
+
+        parameters.insert(0, 0)
+        parameters_index.append(n_iter)
+        ansatz_ops.insert(0, new_op)
+        ansatz_mat.insert(0, new_mat)
+        parameters_mult.insert(0, 1)
+
+        trial_model = tUCCSD(hamiltonian, ansatz_mat, ansatz_ops, ansatz_mat_t, ansatz_ops_t, reference_ket, parameters,
+                             parameters_index, parameters_mult, ansatz_sup_index, ansatz_left, ansatz_right,
+                             ansatz_scal)
+
+        opt_result = scipy.optimize.minimize(trial_model.energy, parameters, jac=trial_model.gradient,
+                                             options=min_options, method='BFGS', callback=trial_model.callback)
+
+        # print(ansatz_ops)
+
+        parameters = list(opt_result['x'])
+        curr_state = trial_model.prepare_state(parameters)
+        overlap = GS.transpose().conj().dot(curr_state)[0, 0]
+        overlap = overlap.real
+        overlap = overlap * overlap
+        # print(" new state ",curr_state)
+        print(" Finished: %20.12f" % trial_model.curr_energy)
+        print(" Overlap: %20.12f" % overlap)
+        print(" Variance: %20.12f" % trial_model.variance(parameters))
+        print(" -----------New ansatz----------- ")
+        # print(curr_state)
+        print(" %4s %30s %12s" % ("Term", "Coeff", "#"))
+        for si in range(len(ansatz_ops)):
+            s = ansatz_ops[si]
+            opstring = ""
+            for t in s.terms:
+                opstring += str(t)
+                break
+            print(" %4s %20f %10s" % (s, parameters[si], si))
+            print(" ")
