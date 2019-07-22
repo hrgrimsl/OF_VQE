d2001de41f1aca1f9d32fb715e95909035ebc57a
holuntang <holuntang@vt.edu>
2019-07-22 17:26:07 -0400

    max_cut 4 nodes

---
+++ examples/max_cut.py
@@ -0,0 +1,13 @@
+import qaoa_methods
+import operator_pools
+import networkx as nx
+import numpy as np
+
+n = 4
+p = 5
+g = nx.Graph()
+g.add_nodes_from(np.arange(0, n, 1))
+elist=[(0, 1, 1.0), (0, 2, 1.0), (0, 3, 1.0), (1, 2, 1.0), (2, 3, 1.0)]
+g.add_weighted_edges_from(elist)
+
+qaoa_methods.qaoa(n, g, layer=p, pool=operator_pools.qaoa())
